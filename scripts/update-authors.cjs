899609b8f81cc4e355aa4ebd22b1c0acd7247c43
Paul Rodwell <paul.rodwell@btinternet.com>
2024-09-27 15:29:02 +0100

    Remove dev dependency on Grunt.

---
+++ scripts/update-authors.cjs
@@ -0,0 +1,15 @@
+const gitAuthors = require("grunt-git-authors");
+
+// list of contributers from prior the split out of Smallest Federated Wiki repo.
+const priorAuthors = [
+  "Ward Cunningham <ward@c2.com>",
+  "Nick Niemeir <nick.niemeir@gmail.com>",
+];
+
+gitAuthors.updateAuthors({ priorAuthors: priorAuthors }, (error, filename) => {
+  if (error) {
+    console.log("Error: ", error);
+  } else {
+    console.log(filename, "updated");
+  }
+});
